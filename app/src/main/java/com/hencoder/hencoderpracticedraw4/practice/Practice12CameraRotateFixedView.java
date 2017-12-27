ffc95e0410b772d797499f9f303687733c4b78a7
Bakumon <bakumon@aliyun.com>
2017-12-27 10:21:26 +0800

    完成练习

--- app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice12CameraRotateFixedView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice12CameraRotateFixedView.java
@@ -3,6 +3,7 @@ package com.hencoder.hencoderpracticedraw4.practice;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Camera;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Point;
@@ -17,6 +18,7 @@ public class Practice12CameraRotateFixedView extends View {
     Bitmap bitmap;
     Point point1 = new Point(200, 200);
     Point point2 = new Point(600, 200);
+    Camera camera;
 
     public Practice12CameraRotateFixedView(Context context) {
         super(context);
@@ -32,13 +34,36 @@ public class Practice12CameraRotateFixedView extends View {
 
     {
         bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.maps);
+        camera = new Camera();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        float centerX1 = bitmap.getWidth() / 2 + point1.x;
+        float centerY1 = bitmap.getHeight() / 2 + point1.y;
+        float centerX2 = bitmap.getWidth() / 2 + point2.x;
+        float centerY2 = bitmap.getHeight() / 2 + point2.y;
+
+        canvas.save();
+        camera.save();
+        camera.rotateX(30);
+        canvas.translate(centerX1, centerY1);
+        camera.applyToCanvas(canvas);
+        canvas.translate(-centerX1, -centerY1);
+        camera.restore();
         canvas.drawBitmap(bitmap, point1.x, point1.y, paint);
+        canvas.restore();
+
+        canvas.save();
+        camera.save();
+        camera.rotateY(30);
+        canvas.translate(centerX2, centerY2);
+        camera.applyToCanvas(canvas);
+        canvas.translate(-centerX2, -centerY2);
+        camera.restore();
         canvas.drawBitmap(bitmap, point2.x, point2.y, paint);
+        canvas.restore();
     }
 }
